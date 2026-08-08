dafdf5b5b87af6df2ce8b2e29ca31e63206b8249
sai-krishna26 <saikrishnavishwakarma@gmail.com>
2026-08-08 21:55:52 +0530

    updated code

---
+++ JavaScript/SuperSimpleDev/script.js
@@ -0,0 +1,22 @@
+/////////////////////////////////////////NUMBERS///////////////////////////////////////////
+// function reverseRound(num)
+// {
+//  let decimal=num%1;
+//     return  decimal>=0.5 ? Math.floor(num) : Math.ceil(num);
+// }
+
+// console.log(reverseRound(8.8));
+
+
+// let temp=-5;
+// console.log(`the temp is ${temp} degree celcius:`,(temp*9/5)+32,'degree fahrenheit');
+// console.log(`the temp is ${temp} degree Fahrenheit:`,((temp-32)*5/9).toFixed(2),'degree celcius');
+
+
+
+//////////////////////////////////////////STRINGS////////////////////////////////////////
+
+
+
+
+
